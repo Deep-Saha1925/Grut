7218389d1b4a5dd546ed9e718fe60743e13a6034
Deep Saha <dip23447@gmail.com>
2026-01-29 23:54:33 +0530

    Added commit function

--- Main.mjs
+++ Main.mjs
@@ -1,6 +1,7 @@
 import path from 'path';
 import fs from 'fs/promises';
 import crypto from 'crypto';
+import { timeStamp } from 'console';
 
 class Grut{
  
@@ -31,11 +32,50 @@ class Grut{
         const fileHash = this.hashObject(fileData);
 
         const newFileHashedObjectPath = path.join(this.objectsPath, fileHash);
+        await this.updateStagingArea(fileToAdd, fileHash);
         await fs.writeFile(newFileHashedObjectPath, fileData);
-        
+
+    }
+
+    async updateStagingArea(filePath, fileHash){
+        const index = JSON.parse(await fs.readFile(this.indexPath, { encoding: 'utf-8'}));
+
+        index.push({ path: filePath, hash: fileHash });
+        await fs.writeFile(this.indexPath, JSON.stringify(index));
+    }
+
+    async commit(message){
+        const index = JSON.parse(await fs.readFile(this.indexPath, { encoding: 'utf-8'}));
+        const parentCommit = await this.getCurrentHead();
+
+        const commitData = {
+            timeStamp: new Date().toISOString(),
+            message: message,
+            parent: parentCommit,
+            files: index
+        };
+
+        const commitHash = this.hashObject(JSON.stringify(commitData));
+        const commitPath = path.join(this.objectsPath, commitHash);
+        await fs.writeFile(commitPath, JSON.stringify(commitData));
+        await fs.writeFile(this.headPath, commitHash);
+        await fs.writeFile(this.indexPath, JSON.stringify([]));
+        console.log(`Committed as ${commitHash}`);
+
+    }
+
+    async getCurrentHead(){
+        try{
+            return await fs.readFile(this.headPath, { encoding: 'utf-8' });
+        } catch (e) {
+            return null;
+        }
     }
 
 }
 
-
-const grut = new Grut();
+(async () => {
+    const grut = new Grut();
+    await grut.add('test.txt');
+    await grut.commit('Initial commit');
+})();
