17ebc28869d5df731bdd97557751ebd6b2c664c6
Deep Saha <dip23447@gmail.com>
2026-01-29 23:36:57 +0530

    Added hash function and add file function

--- Main.mjs
+++ Main.mjs
@@ -1,5 +1,6 @@
 import path from 'path';
 import fs from 'fs/promises';
+import crypto from 'crypto';
 
 class Grut{
  
@@ -14,10 +15,27 @@ class Grut{
     async init(){
         await fs.mkdir(this.objectsPath, { recursive: true });
         try{
-            
+            await fs.writeFile(this.headPath, '', { flag: 'wx' });
+            await fs.writeFile(this.indexPath, JSON.stringify([]), { flag: 'wx' });
         } catch (e) {
-
+            console.log('Repository already initialized.');
         }
     }
 
+    hashObject(data){
+        return crypto.createHash('sha1').update(data, 'utf-8').digest('hex');
+    }
+
+    async add(fileToAdd){
+        const fileData = await fs.readFile(fileToAdd, 'utf-8');
+        const fileHash = this.hashObject(fileData);
+
+        const newFileHashedObjectPath = path.join(this.objectsPath, fileHash);
+        await fs.writeFile(newFileHashedObjectPath, fileData);
+        
+    }
+
 }
+
+
+const grut = new Grut();
